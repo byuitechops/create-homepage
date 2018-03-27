4996a8809d1fef2ddd481e6f1d7ab9ef29c07162
Zachary  Williams <wil11063@byui.edu>
2018-03-27 10:13:40 -0600

    Update childTests.js

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -1,23 +1,16 @@
-/*eslint-env node, es6*/
-
 /* Dependencies */
 const tap = require('tap');
+const canvas = require('canvas-wrapper');
 
-function g1Tests(course, callback) {
-    
-    var changes = course.report.find(report => report.name === 'create-homepage').changes;
+module.exports = (course, callback) => {
+    tap.test('child-template', (test) => {
 
-    tap.equal(changes[0], 'Retrieved Homepage template.');
-    tap.equal(changes[1], 'Found and inserted course banner into Homepage.');
-    tap.equal(changes[2], 'Course Homepage successfully created with the template.');
-    tap.equal(changes[3], 'Course Default View set to the Front Page.');
+        test.pass('potato');
+        test.pass('tomato');
+        test.fail('avacado');
+
+        test.end();
+    });
 
     callback(null, course);
-}
-
-module.exports = [
-        {
-            gauntlet: 1,
-            tests: g1Tests
-        }
-];
+};
