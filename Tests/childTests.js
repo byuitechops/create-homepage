ca6635bda2d1cf1d3f0011cf2bd1a23f4bf6af77
Zachary Williams <wil11063@byui.edu>
2018-04-24 09:32:52 -0600

    Add tests

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -3,13 +3,37 @@ const tap = require('tap');
 const canvas = require('canvas-wrapper');
 
 module.exports = (course, callback) => {
-    tap.test('child-template', (test) => {
+    tap.test('create-homepage', (test) => {
 
-        test.pass('potato');
-        test.pass('tomato');
-        test.fail('avacado');
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/front_page`, (err, homepageArr) => {
+            if (err) {
+                course.error(err);
+                test.end();
+                return;
+            }
 
-        test.end();
+            var homepage = homepageArr[0];
+
+            canvas.get(`/api/v1/courses/${course.info.canvasOU}`, (err, courseArr) => {
+                if (err) {
+                    course.error(err);
+                    test.end();
+                    return;
+                }
+
+                var canvasCourse = courseArr[0];
+
+                test.ok(homepage, 'Homepage does not exist');
+                test.ok(homepage.front_page, 'The page retrieved is not set as the home page');
+                test.ok(homepage.published, 'The homepage is not published');
+                test.ok(/homeImage\.jpg/.test(homepage.body), 'The homeImage isn\'t set');
+                test.equal(homepage.hide_from_students, false, 'The homepage is hidden from students');
+                test.equal(homepage.title, '-Course Homepage', 'The title is not set to "-Course Homepage"');
+                test.equal(homepage.editing_roles, 'teachers', 'The editing roles are not set to just "teachers"');
+                test.equal(canvasCourse.default_view, 'wiki', 'Default view for the course isn\'t set to the homepage');
+                test.end();
+            });
+        });
     });
 
     callback(null, course);
