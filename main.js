7f30ebcd8c26904c43a6fc64cb507b33ec09b476
Zachary Williams <wil11063@byui.edu>
2018-04-25 10:53:50 -0600

    Initial redesign

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-/* Module Description */
-/* Creates a homepage in the course, but does not populate it */
-
-const https = require('https');
+const request = require('request');
 const canvas = require('canvas-wrapper');
 const asyncLib = require('async');
+const cheerio = require('cheerio');
 
 module.exports = (course, stepCallback) => {
 
@@ -17,75 +15,78 @@ module.exports = (course, stepCallback) => {
 
     /* Get the template from equella */
     function getTemplate(callback) {
-        // UNOFFICIAL EQUELLA URL: https://content.byui.edu/file/a397b063-c8bd-4e86-87a3-04a6ac57b49c/$/home.html
-        /* GitHub URL to byui-design-lti home page template */
-        https.get('https://raw.githubusercontent.com/byuitechops/byui-design-lti/master/views/homePage.ejs', (res) => {
+        // /* GitHub URL to byui-design-lti home page template */
+        // https.get('https://raw.githubusercontent.com/byuitechops/byui-design-lti/master/views/homePage.ejs', (res) => {
 
-            /* When we receive the homepage template, send it on */
-            res.on('data', (d) => {
-                course.message('Retrieved Homepage Template');
-                callback(null, d.toString());
-            });
+        //     /* When we receive the homepage template, send it on */
+        //     res.on('data', (d) => {
+        //         course.message('Retrieved Homepage Template');
+        //         callback(null, d.toString());
+        //     });
 
-        }).on('error', (err) => {
-            callback(err, null);
+        // }).on('error', (err) => {
+        //     callback(err, null);
+        // });
+
+        request('https://raw.githubusercontent.com/byuitechops/byui-design-lti/master/views/homePage.ejs', (err, res, body) => {
+            if (err) {
+                callback(err);
+                return;
+            }
+            course.message('Retrieved Homepage Template');
+            callback(null, body);
         });
     }
 
     /* Update the template using course information */
     function updateTemplate(template, callback) {
 
-        /* Put the course name into place */
+        /* Replace things easily identified with regex */
         template = template.replace(/<%=\s*courseName\s*%>/gi, course.info.courseCode);
         template = template.replace(/<%=\s*courseClass\s*%>/gi, course.info.courseCode.replace(/\s/g, ''));
-
-        /* add the generate class to the lessons container */
-        template = template.replace(/<div class="lessons">/, '<div class="lessons generate">');
-
-        /* remove the welcome class */
-        // TODO should this be using cheerio?
-
-        /* Assumes reorganize file structure has/will run. reorganize file structure DOES NOT have to run first */
-        template = template.replace(/img src=".*"/gi,
-            `img src="https://${course.info.domain}.instructure.com/courses/${course.info.canvasOU}/file_contents/course%20files/template/homeImage.jpg"`);
-        course.message('Found and inserted course banner into Homepage');
-
-        /* Put the course description instruction bit into the template */
         template = template.replace(/\[Lorem.*\]/gi, '[Course Description goes here]');
-        /* Send back the updated template */
+
+        var $ = cheerio.load(template);
+
+        /* Add the generate class */
+        $('.lessons').addClass('generate');
+
+        /* Add the homeImage src */
+        $('img').attr('src', `https://${course.info.domain}.instructure.com/courses/${course.info.canvasOU}/file_contents/course%20files/template/homeImage.jpg`);
+
         callback(null, template);
     }
 
     /* Create the Front Page */
     function createFrontPage(template, callback) {
         canvas.put(`/api/v1/courses/${course.info.canvasOU}/front_page`, {
-            'wiki_page[title]': '-Course Homepage',
-            'wiki_page[body]': template,
-            'wiki_page[editing_roles]': 'teachers',
-            'wiki_page[published]': true
-        },
-        (err, page) => {
-            if (err) callback(err, page);
-            else {
-                course.message('Course Homepage successfully created with the template');
-                callback(null, page);
-            }
-        });
+                'wiki_page[title]': '-Course Homepage',
+                'wiki_page[body]': template,
+                'wiki_page[editing_roles]': 'teachers',
+                'wiki_page[published]': true
+            },
+            (err, page) => {
+                if (err) callback(err, page);
+                else {
+                    course.message('Course Homepage successfully created with the template');
+                    callback(null, page);
+                }
+            });
     }
 
     function updateHomepage(page, callback) {
         /* This API call is not on the official docs, but here is a thread
            explaining it: https://community.canvaslms.com/thread/11645 */
         canvas.put(`/api/v1/courses/${course.info.canvasOU}`, {
-            'course[default_view]': 'wiki'
-        },
-        (err, canvasCourse) => {
-            if (err) callback(err, canvasCourse);
-            else {
-                course.message('Course Default View set to the Front Page');
-                callback(null, canvasCourse);
-            }
-        });
+                'course[default_view]': 'wiki'
+            },
+            (err, canvasCourse) => {
+                if (err) callback(err, canvasCourse);
+                else {
+                    course.message('Course Default View set to the Front Page');
+                    callback(null, canvasCourse);
+                }
+            });
     }
 
     var validPlatforms = ['online', 'pathway'];
