deaa8dca8452cce94614b49897e6515409df1a4f
Wilson <kilakal@byui.edu>
2018-06-21 14:18:58 -0600

    Added multiple homepage templates for campus courses.

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 const request = require('request');
-const fs = require('fs');
 const canvas = require('canvas-wrapper');
 const asyncLib = require('async');
 const cheerio = require('cheerio');
+
+let templates = {
+    'Basic': 'basic',
+    'Basic Details': 'basic-details',
+    'Lg Ends Details': 'large-ends-details',
+    'Lg Ends Plain': 'large-ends-plain',
+    'Modules': 'modules',
+    'Other': 'modules',
+    'Full Pictures': 'picture-buttons-full-example',
+    'Schedule': 'detail-template-mwf',
+    'Small Pictures': 'picture-buttons-small-example',
+    'Sm Weeks Auto': 'default-template-small',
+    'Syllabus': 'syllabus',
+    'Weeks Auto': 'default-template-large'
+};
+
+let tabs = ['other', 'modules', 'syllabus'];
+
 
 module.exports = (course, stepCallback) => {
 
@@ -17,18 +34,31 @@ module.exports = (course, stepCallback) => {
     /* Get the template from equella */
     function getTemplate(callback) {
         if (course.info.platform === 'campus') {
-            // Campus courses have more than 1 template to choose from. The template will be chosen on startup and be stored on the course object.
-            // The templates should be stored locally, so a path to that local file will be necessary.
+            if (!tabs.includes(course.info.campusTemplate.toLowerCase())) {
 
-            // TODO: Give it the path to the correct template
-            fs.readFile(`/campusTemplates/${course.info.campusTemplate}`, (err, body) => {
-                if (err) {
-                    callback(err);
-                    return;
-                }
-                course.message('Retrieved Campus Homepage Template');
-                callback(null, body);
-            });
+                // Campus courses have more than 1 template to choose from. The template will be chosen on startup and be stored on the course object.
+                // The templates are stored here: https://byui.instructure.com/courses/16631/pages. Use the canvas-wrapper to get all the pages and select the template.
+
+                canvas.get(`/api/v1/courses/16631/pages/${templates[course.info.campusTemplate]}`, (err, page) => {
+                    if (err) {
+                        callback(err);
+                        return;
+                    }
+                    course.message(`Retrieved ${course.info.campusTemplate} Template`);
+                    callback(null, page[0].body);
+                });
+            } else {
+                // Set the homepage to be either the syllabus or modules tab
+                canvas.put(`/api/v1/courses/${course.info.canvasOU}`, {
+                    'course[default_view]': templates[course.info.campusTemplate]
+                }, (err, canvasCourse) => {
+                    if (err) stepCallback(err, course);
+                    else {
+                        course.message(`"${templates[course.info.campusTemplate]}" set as the Front Page`);
+                        stepCallback(null, course);
+                    }
+                });
+            }
         } else {
             request('https://raw.githubusercontent.com/byuitechops/byui-design-lti/master/views/homePage.ejs', (err, res, body) => {
                 if (err) {
@@ -45,21 +75,26 @@ module.exports = (course, stepCallback) => {
     function updateTemplate(template, callback) {
 
         /* Replace things easily identified with regex */
-        template = template.replace(/<%=\s*courseName\s*%>/gi, course.info.courseCode);
-        template = template.replace(/<%=\s*courseClass\s*%>/gi, course.info.courseCode.replace(/\s/g, '').toLowerCase());
-        template = template.replace(/\[Lorem.*\]/gi, '[Course Description goes here]');
-        template = template.replace(/Additional\sResources/gi, 'Student Resources');
-
         var $ = cheerio.load(template);
+        if (course.info.platform !== 'campus') {
+            // Online
+            template = template.replace(/<%=\s*courseName\s*%>/gi, course.info.courseCode);
+            template = template.replace(/<%=\s*courseClass\s*%>/gi, course.info.courseCode.replace(/\s/g, '').toLowerCase());
+            template = template.replace(/\[Lorem.*\]/gi, '[Course Description goes here]');
+            template = template.replace(/Additional\sResources/gi, 'Student Resources');
 
-        /* Add the generate class */
-        $('.lessons').addClass('generate'); // does this work?
+            /* Add the generate class */
+            $('.lessons').addClass('generate'); // does this work?
 
-        /* Add the homeImage src */
-        $('img').attr('src', `https://${course.info.domain}.instructure.com/courses/${course.info.canvasOU}/file_contents/course%20files/template/homeImage.jpg`);
+            /* Add the homeImage src */
+            $('img').attr('src', `https://${course.info.domain}.instructure.com/courses/${course.info.canvasOU}/file_contents/course%20files/template/homeImage.jpg`);
 
-        template = $.html();
+            template = $.html();
 
+        } else {
+            // Campus
+            // Add edits to the template here
+        }
         callback(null, template);
     }
 
@@ -74,7 +109,7 @@ module.exports = (course, stepCallback) => {
         (err, page) => {
             if (err) callback(err, page);
             else {
-                course.message('Course Homepage successfully created with the template');
+                course.message(`Course Homepage successfully created with the ${course.info.campusTemplate} template`);
                 callback(null, page);
             }
         });
@@ -89,14 +124,18 @@ module.exports = (course, stepCallback) => {
         (err, canvasCourse) => {
             if (err) callback(err, canvasCourse);
             else {
-                course.message('Course Default View set to the Front Page');
+                course.message(`"${course.info.campusTemplate}" template set as the Front Page`);
                 callback(null, canvasCourse);
             }
         });
     }
 
-    /* Waterfall our functions so we can keep it all organized */
+
+    // Check if the template is a wiki page or a course tab
+
     asyncLib.waterfall(functions, (err, result) => {
+        // The homepage will be a wiki page
+        /* Waterfall our functions so we can keep it all organized */
         if (err) {
             course.error(err);
             stepCallback(null, course);
